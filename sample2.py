b2a624442be241f24fcdec69bd3f77bb07deacbb
b1013251 <b1013251@gmail.com>
2016-04-21 17:21:55 +0900

    output delta

--- sample2.py
+++ sample2.py
@@ -4,25 +4,38 @@ import numpy as np
 import math
 
 # 教師データ
-train = ((
+train = (
 	(0,0,0),
 	(0,1,1),
 	(1,0,1),
 	(1,1,0)
-))
+)
 
 # 列挙型もどき
 ( INPUT, HIDDEN, OUTPUT ) = range(3)
+
+class Calc :
+	# シグモイド関数
+	@classmethod
+	def sigmoid(self, x ) :
+		return 1.0 / (1.0 + math.exp(-x))
+
+	# シグモイドの微分
+	@classmethod
+	def sigmoid_dash(self, x) :
+		return (1 - self.sigmoid(x)) * self.sigmoid(x)
 
 class Path :
 	def __init__(self,weight,back,forward):
 		self.back = back
 		self.forward = forward
 		self.weight = weight
+		self.delta = 0
 
 class Neuron :
 	def __init__(self,value,ntype,num):
 		self.value = value
+		self.raw_value = 0
 		self.neuron_type = ntype
 		self.num = num
 
@@ -50,7 +63,8 @@ class Neuron :
 			#print "result +", p.weight , "*", nn.neurons[p.forward].value, "(", nn.neurons[p.forward].num ,")"
 			result += p.weight * nn.neurons[p.back].value
 
-		self.value = result
+		self.raw_value = result
+		self.value = Calc.sigmoid(result)
 
 class NN :
 	def __init__(self,input_num,hidden_num,output_num) :
@@ -96,12 +110,37 @@ class NN :
 
 		return (y1, y2)
 
-	# シグモイド関数
-	def sigmoid(self, x )
-		return 1.0 / (1.0 + math.exp(-x))
+
+	# デルタ
+	def output_delta(self, path, train, value, raw_value) :
+		path.delta = (train[2] - value) * Calc.sigmoid_dash(raw_value)
+		print "path.delta", path.delta
+
+	def hidden_delta(self, pathes) :
+		neuron = nn.neurons[pathes.back]
+		sum = 0
+		for p in pathes :
+			sum += p.delta * p.weight
+		delta = Calc.sigmoid_dash(neuron.raw_value)
+
 
 	# 誤差逆伝搬を行う
+	def bp_step(self,train) :
+		for n in reversed(self.neurons) :
+			if n.neuron_type == OUTPUT :
+				a_path = n.search_back(n.num,self.pathes)[0]
+				self.output_delta(a_path,train,n.value,n.raw_value)
+			if n.neuron_type == HIDDEN :
+				print "hidden"
+			if n.neuron_type == INPUT :
+				print "input"
 
+	# 誤差逆伝播を繰り返す
+	def backpropergation(self,n) :
+		for step in xrange(0, n + 1) :
+			self.bp_step(train[0])
+			if step % (n / 10) == 0:
+				print "step:", step
 
 
 if __name__ == "__main__":
@@ -110,3 +149,5 @@ if __name__ == "__main__":
 	nn = NN(2,3,2)
 	y = nn.output()
 	print y
+
+	nn.backpropergation(100)
